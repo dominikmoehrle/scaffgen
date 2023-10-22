7a786181962babe9489dea4af189d82af969d215
Dominik <dominik.moehrle@gmail.com>
2023-10-22 15:31:11 -0700

    fixed deployment errors

--- src/app/start/[id]/page.tsx
+++ src/app/start/[id]/page.tsx
@@ -1,75 +1,83 @@
-import { kv } from '@vercel/kv';
-import { notFound } from 'next/navigation';
-import { Metadata } from 'next';
-import Body from '@/components/Body';
+//import { kv } from "@vercel/kv";
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import Body from "@/components/Body";
 
-async function getAllKv(id: string) {
-  const data = await kv.hgetall<{
-    prompt: string;
-    image?: string;
-    website_url?: string;
-    model_latency?: string;
-  }>(id);
+import React from "react";
 
-  return data;
-}
+const Page: React.FC = () => {
+  return <div>Hello, world!</div>;
+};
 
-export async function generateMetadata({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}): Promise<Metadata | undefined> {
-  const data = await getAllKv(params.id);
-  if (!data) {
-    return;
-  }
+export default Page;
 
-  const title = `QrGPT: ${data.prompt}`;
-  const description = `A QR code generated from qrGPT.io linking to: ${data.website_url}`;
-  const image = data.image || 'https://qrGPT.io/og-image.png';
+// async function getAllKv(id: string) {
+//   const data = await kv.hgetall<{
+//     prompt: string;
+//     image?: string;
+//     website_url?: string;
+//     model_latency?: string;
+//   }>(id);
 
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      images: [
-        {
-          url: image,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description,
-      images: [image],
-      creator: '@nutlope',
-    },
-  };
-}
+//   return data;
+// }
 
-export default async function Results({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) {
-  const data = await getAllKv(params.id);
-  if (!data) {
-    notFound();
-  }
-  return (
-    <Body
-      prompt={data.prompt}
-      imageUrl={data.image}
-      redirectUrl={data.website_url}
-      modelLatency={Number(data.model_latency)}
-      id={params.id}
-    />
-  );
-}
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }): Promise<Metadata | undefined> {
+//   const data = await getAllKv(params.id);
+//   if (!data) {
+//     return;
+//   }
+
+//   const title = `QrGPT: ${data.prompt}`;
+//   const description = `A QR code generated from qrGPT.io linking to: ${data.website_url}`;
+//   const image = data.image || "https://qrGPT.io/og-image.png";
+
+//   return {
+//     title,
+//     description,
+//     openGraph: {
+//       title,
+//       description,
+//       images: [
+//         {
+//           url: image,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title,
+//       description,
+//       images: [image],
+//       creator: "@nutlope",
+//     },
+//   };
+// }
+
+// export default async function Results({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }) {
+//   const data = await getAllKv(params.id);
+//   if (!data) {
+//     notFound();
+//   }
+//   return (
+//     <Body
+//       prompt={data.prompt}
+//       imageUrl={data.image}
+//       redirectUrl={data.website_url}
+//       modelLatency={Number(data.model_latency)}
+//       id={params.id}
+//     />
+//   );
+// }
