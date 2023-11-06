8d58d738a05d75de1ba15d557c2286dd5978bc60
Dominik <dominik.moehrle@gmail.com>
2023-11-05 19:18:05 -0800

    got everything working

--- src/utils/supabaseClient.tsx
+++ src/utils/supabaseClient.tsx
@@ -4,16 +4,9 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (supabaseUrl === undefined || supabaseAnonKey === undefined) {
-  console.log("supabaseUrl is: " + supabaseUrl);
-  console.log("supabaseAnonKey is: " + supabaseAnonKey);
   throw new Error("Env variables are incorrectly loaded");
 }
-
-console.log("supabaseUrl is: " + supabaseUrl);
-console.log("supabaseAnonKey is: " + supabaseAnonKey);
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
-
-//Client for Database
